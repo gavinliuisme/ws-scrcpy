6388c5460b15596edb6b63d7bbcbc25f375a450c
星峰 <34774269+gavinliuisme@users.noreply.github.com>
2026-04-01 20:39:18 +0800

    Update GoogToolBox.ts

--- src/app/googDevice/toolbox/GoogToolBox.ts
+++ src/app/googDevice/toolbox/GoogToolBox.ts
@@ -104,8 +104,8 @@ export class GoogToolBox extends ToolBox {
             client.sendMessage(CommandControlMessage.createSetScreenPowerModeCommand(screenPowerOn));            
             // 2. 如果是打开屏幕，发送 Menu 键唤醒设备（不锁屏）
             if (screenPowerOn) {
-                const menuDown = new KeyCodeControlMessage(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MENU, 0, 0);
-                const menuUp = new KeyCodeControlMessage(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MENU, 0, 0);                
+                const menuDown = new KeyCodeControlMessage(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_WAKEUP, 0, 0);
+                const menuUp = new KeyCodeControlMessage(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_WAKEUP, 0, 0);                
                 client.sendMessage(menuDown);
                 setTimeout(() => {
                     client.sendMessage(menuUp);
